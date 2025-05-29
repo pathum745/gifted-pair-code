5aabb26eb6f349212052bdd12cb219f5faa28fa9
pathum745 <pathummalsara74@gmail.com>
2025-05-29 21:00:37 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -30,7 +30,7 @@ async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
             email: 'pathummalsara4@gmail.com', // Your Mega A/c Email Here
-            password: 'pathum12345' // Your Mega A/c Password Here
+            password: 'pathum123456' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
